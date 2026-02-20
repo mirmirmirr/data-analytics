f1feb6e15170e0bb9ebeda432a531644ad10bc46
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-02-20 09:38:10 -0500

    initial lab 3

---
+++ labs/lab 3/Exercise_1.R
@@ -0,0 +1,7 @@
+## Train and evaluate 2 kNN models using the abalone dataset
+## Each model should use a different subset of features
+
+## Compare models using contingency tables
+## Find the optimal value k for the better performing model from above by
+## training over a range of k values and choosing the value with the
+## highest accuracy (accuracy = correct classifications / total observations)
---
+++ labs/lab 3/Exercise_2.R
@@ -0,0 +1,5 @@
+## Use k-means and PAM to find clusters in the abalone dataset
+## using the feature subset from the exercise 1.
+
+## Find the optimal value k for each model and plot silhouette plots
+## for both models with their optimum k.
